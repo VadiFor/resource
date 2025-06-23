b14c36e031b32b1ba3319db01261f33e57e23210
Stepanov Vadim <v.stepanov@rdsystems.ru>
2025-06-24 00:13:43 +0700

    REST v2 Отредактирован pom: добавлена зависимость springdoc-openapi-starter-webmvc-ui, для совместимости понижена версия spring boot. Добавлены аннотации и описания для swagger - контроллеру и dto.

--- src/main/java/com/learn/java/controller/ResourceController.java
+++ src/main/java/com/learn/java/controller/ResourceController.java
@@ -4,6 +4,8 @@ import com.learn.java.dto.ResourceCreateRequestDto;
 import com.learn.java.dto.ResourceUpdateRequestDto;
 import com.learn.java.model.Resource;
 import com.learn.java.service.ResourceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,31 +14,37 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/res")
+@Tag(name = "Resource Controller")
 public class ResourceController {
 	private final ResourceService resourceService;
 	
 	@GetMapping
+	@Operation(summary = "Get all resources")
 	public List<Resource> getAllResource() {
 		return resourceService.getAllResources();
 	}
 	
 	@GetMapping("/{id}")
+	@Operation(summary = "Get resource by ID")
 	public Resource getResource(@PathVariable String id) {
 		return resourceService.getById(id);
 	}
 	
 	@PostMapping
+	@Operation(summary = "Create resource")
 	public Resource createResource(@RequestBody ResourceCreateRequestDto resourceCreateRequestDto) {
 		return resourceService.create(resourceCreateRequestDto);
 	}
 	
 	@PatchMapping("/{id}")
+	@Operation(summary = "Update resource")
 	public Resource updateResource(@PathVariable String id,
 								   @RequestBody ResourceUpdateRequestDto resourceUpdateRequestDto) {
 		return resourceService.update(id, resourceUpdateRequestDto);
 	}
 	
 	@DeleteMapping("/{id}")
+	@Operation(summary = "Delete resource")
 	public String deleteResource(@PathVariable String id) {
 		return resourceService.delete(id);
 	}
--- src/main/java/com/learn/java/dto/ResourceUpdateRequestDto.java
+++ src/main/java/com/learn/java/dto/ResourceUpdateRequestDto.java
@@ -2,6 +2,7 @@ package com.learn.java.dto;
 
 import com.learn.java.model.enums.StatusResource;
 import com.learn.java.model.enums.TypeResource;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +13,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResourceUpdateRequestDto {
+	@Schema(description = "Name resource", example = "ROOM A")
 	private String name;
+	@Schema(description = "Type resource", example = "ROOM")
 	private TypeResource type;
+	@Schema(description = "Location resource", example = "Office A")
 	private String location;
+	@Schema(description = "Status resource", example = "ACTIVE")
 	private StatusResource status;
+	@Schema(description = "Resource portable", example = "false")
 	private Boolean portable;
+	@Schema(description = "Resource requires approval", example = "true")
 	private Boolean requiresApproval;
+	@Schema(description = "Resource shared", example = "true")
 	private Boolean isShared;
 }
